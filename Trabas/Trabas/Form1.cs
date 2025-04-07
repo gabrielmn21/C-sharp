c3de31eb477410f5779908dd701218cb04341337
gabrielmn21 <gm771557@gmail.com>
2025-04-07 19:42:33 -0300

    Add files via upload

---
+++ Trabas/Trabas/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void usersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.usersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.gabriel_BDDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'gabriel_BDDataSet.Users'. Você pode movê-la ou removê-la conforme necessário.
+            this.usersTableAdapter.Fill(this.gabriel_BDDataSet.Users);
+
+        }
+    }
+}
---
+++ Trabas/Trabas/Gabriel_BDDataSet.cs
@@ -0,0 +1,18 @@
+﻿namespace Trabas
+{
+}
+
+namespace Trabas
+{
+
+
+    public partial class Gabriel_BDDataSet
+    {
+    }
+}
+namespace Trabas {
+    
+    
+    public partial class Gabriel_BDDataSet {
+    }
+}
